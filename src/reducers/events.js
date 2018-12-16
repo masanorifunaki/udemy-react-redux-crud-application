065b569e4a349acc347b9bb40b25963de0d71962
舟木正憲 <funakimfunaki@gmail.com>
2018-12-17 08:29:24 +0900

    implement eventsShow

--- src/reducers/events.js
+++ src/reducers/events.js
@@ -1,8 +1,13 @@
-import _                              from 'lodash';
-import { DELETE_EVENTS, READ_EVENTS } from '../actions';
+import _                                                                       from 'lodash';
+import { CREATE_EVENTS, DELETE_EVENTS, READ_EVENT, READ_EVENTS, UPDATE_EVENT } from '../actions';
 
 export default (events = {}, action) => {
-  switch (action.type) {
+  switch ( action.type ) {
+    case READ_EVENT:
+    case UPDATE_EVENT:
+    case CREATE_EVENTS:
+      const data = action.response.data;
+      return { ...events, [data.id]: data };
     case READ_EVENTS:
       return _.mapKeys(action.response.data, 'id');
     case DELETE_EVENTS:
